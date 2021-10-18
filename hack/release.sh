90e1cd554380299e5e8cbf0c1774dcdc32553c80
Jason Hall <jasonhall@redhat.com>
2021-10-18 14:19:59 -0400

    WIP: Demonstrate signing release images

--- hack/release.sh
+++ hack/release.sh
@@ -6,8 +6,8 @@
 
 set -euo pipefail
 
-echo "Logging into container registry $IMAGE_HOST"
-echo "$REGISTRY_PASSWORD" | ko login -u "$REGISTRY_USERNAME" --password-stdin "$IMAGE_HOST"
+#echo "Logging into container registry $IMAGE_HOST"
+#echo "$REGISTRY_PASSWORD" | ko login -u "$REGISTRY_USERNAME" --password-stdin "$IMAGE_HOST"
 
 echo "Building container image"
 
@@ -20,13 +20,13 @@ KO_DOCKER_REPO="${IMAGE_HOST}/${IMAGE_NAMESPACE}" GOFLAGS="${GO_FLAGS}" ko resol
   --base-import-paths \
   --tags "${TAG}" \
   --image-label "io.shipwright.vcs-ref=${GITHUB_SHA}" \
-  --platform=all -R -f deploy/ > release.yaml
+  -R -f deploy/ > release.yaml
 
 KO_DOCKER_REPO="${IMAGE_HOST}/${IMAGE_NAMESPACE}" GOFLAGS="${GO_FLAGS} -tags=pprof_enabled" ko resolve \
   --base-import-paths \
   --tags "${TAG}-debug" \
   --image-label "io.shipwright.vcs-ref=${GITHUB_SHA}" \
-  --platform=all -R -f deploy/ > release-debug.yaml
+  -R -f deploy/ > release-debug.yaml
 
 # Bundle the sample cluster build strategies, remove namespace strategies first
 find samples/buildstrategy -type f -print0 | xargs -0 grep -l "kind: BuildStrategy" | xargs rm -f
